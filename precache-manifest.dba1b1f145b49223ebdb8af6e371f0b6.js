6da32ee1660c1429f61792ddc86eb922bd392f6e
Donald Ty Lam <dlamty@gmail.com>
2018-11-21 16:25:42 +0000

    Add files via upload

---
+++ precache-manifest.dba1b1f145b49223ebdb8af6e371f0b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ee23ecb4fc2801ce76",
+    "url": "/tfgm-telescope/static/js/runtime~main.11ee23ec.js"
+  },
+  {
+    "revision": "404b5a9c8cee03b7d750",
+    "url": "/tfgm-telescope/static/js/main.404b5a9c.chunk.js"
+  },
+  {
+    "revision": "154ee15fdd79cc1c0fcb",
+    "url": "/tfgm-telescope/static/js/1.154ee15f.chunk.js"
+  },
+  {
+    "revision": "404b5a9c8cee03b7d750",
+    "url": "/tfgm-telescope/static/css/main.d6cbe6e7.chunk.css"
+  },
+  {
+    "revision": "1fba1cb82385d6eb3eac63ae0239b47d",
+    "url": "/tfgm-telescope/index.html"
+  }
+];
